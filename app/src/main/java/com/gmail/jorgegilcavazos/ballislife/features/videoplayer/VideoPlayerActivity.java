5f45e967f0f7b3832983f4ec263b15bb8a1885de
jorgegil96 <jorgegilcavazos@gmail.com>
2017-05-28 17:38:38 -0700

    Apply dark theme to video player activity

--- app/src/main/java/com/gmail/jorgegilcavazos/ballislife/features/videoplayer/VideoPlayerActivity.java
+++ app/src/main/java/com/gmail/jorgegilcavazos/ballislife/features/videoplayer/VideoPlayerActivity.java
@@ -1,8 +1,8 @@
 package com.gmail.jorgegilcavazos.ballislife.features.videoplayer;
 
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.afollestad.easyvideoplayer.EasyVideoCallback;
