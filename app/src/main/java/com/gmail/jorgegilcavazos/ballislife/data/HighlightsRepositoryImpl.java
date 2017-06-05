c69f30c438eb535eaba5464904173854443d3da6
jorgegil96 <jorgegilcavazos@gmail.com>
2017-06-04 20:45:31 -0700

    Fix bug in state restoring for highlights

--- app/src/main/java/com/gmail/jorgegilcavazos/ballislife/data/HighlightsRepositoryImpl.java
+++ app/src/main/java/com/gmail/jorgegilcavazos/ballislife/data/HighlightsRepositoryImpl.java
@@ -45,6 +45,7 @@ public class HighlightsRepositoryImpl implements HighlightsRepository {
     public void reset() {
         firstLoad = true;
         lastHighlighKey = "";
+        cachedHighlights.clear();
     }
 
     @Override
