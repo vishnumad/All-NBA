85105a7608915e1ca3bb3d423901efa5783ba5b4
jorgegil96 <jorgegilcavazos@gmail.com>
2017-06-30 14:40:14 -0700

    Setup LeakCanary

--- app/src/main/java/com/gmail/jorgegilcavazos/ballislife/features/application/BallIsLifeApplication.java
+++ app/src/main/java/com/gmail/jorgegilcavazos/ballislife/features/application/BallIsLifeApplication.java
@@ -8,6 +8,7 @@ import com.gmail.jorgegilcavazos.ballislife.dagger.component.AppComponent;
 import com.gmail.jorgegilcavazos.ballislife.dagger.component.DaggerAppComponent;
 import com.gmail.jorgegilcavazos.ballislife.dagger.module.AppModule;
 import com.gmail.jorgegilcavazos.ballislife.dagger.module.DataModule;
+import com.squareup.leakcanary.LeakCanary;
 
 import jonathanfinerty.once.Once;
 
@@ -15,11 +16,22 @@ public class BallIsLifeApplication extends Application {
 
     private static AppComponent appComponent;
 
+    public static AppComponent getAppComponent() {
+        return appComponent;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
         Once.initialise(this);
+
         appComponent = DaggerAppComponent.builder()
                 .appModule(new AppModule(this))
                 .dataModule(new DataModule("https://nba-app-ca681.firebaseio.com/"))
@@ -30,9 +42,5 @@ public class BallIsLifeApplication extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
-    }
-
-    public static AppComponent getAppComponent() {
-        return appComponent;
     }
 }
