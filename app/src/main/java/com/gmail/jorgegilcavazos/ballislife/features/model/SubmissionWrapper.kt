b9fe6a0468a9e560b1eee0f2230f31d5d3f2c35a
Vishnu <vmadhusoodanan95@gmail.com>
2018-10-03 21:27:22 -0500

    Fix bug where NullPointerException is thrown when loading posts containing Reddit embedded videos

--- app/src/main/java/com/gmail/jorgegilcavazos/ballislife/features/model/SubmissionWrapper.kt
+++ app/src/main/java/com/gmail/jorgegilcavazos/ballislife/features/model/SubmissionWrapper.kt
@@ -1,6 +1,7 @@
 package com.gmail.jorgegilcavazos.ballislife.features.model
 
 import net.dean.jraw.models.CommentSort
+import net.dean.jraw.models.OEmbed
 import net.dean.jraw.models.Submission
 import net.dean.jraw.models.VoteDirection
 
@@ -37,7 +38,7 @@ data class SubmissionWrapper(val id: String,
     score = submission?.score ?: 0
     commentCount = submission?.commentCount ?: 0
     thumbnail = submission?.thumbnail
-    highResThumbnail = submission?.oEmbedMedia?.thumbnail?.url?.toString() ?: ""
+    highResThumbnail = getOEmbedMedia()?.thumbnail?.url?.toString() ?: ""
     voteDirection = submission?.vote
     isSaved = submission?.isSaved == true
     selfTextHtml = submission?.data("selftext_html")
@@ -46,4 +47,24 @@ data class SubmissionWrapper(val id: String,
 
   constructor(submission: Submission) :
       this(submission.id, submission, submission.title, submission.author)
+
+  /**
+   * Get OEmbed media ourselves. [Submission.getOEmbedMedia] only checks if "media"
+   * exists and does not make sure that an "oembed" field exists before creating an [OEmbed]
+   * object causing NullPointerExceptions if the "oembed" field does not exist.
+   *
+   * Reddit posts containing embedded videos do not have an "oembed" field.
+   */
+  private fun getOEmbedMedia(): OEmbed? {
+    return submission?.dataNode?.run {
+      when {
+        // Same as in [Submission.getOEmbedMedia]
+        has("media").not() -> null
+        get("media").size() == 0 -> null
+        // Check that the node contains an "oembed" field
+        get("media").has("oembed") -> OEmbed(get("media").get("oembed"))
+        else -> null
+      }
+    }
+  }
 }
